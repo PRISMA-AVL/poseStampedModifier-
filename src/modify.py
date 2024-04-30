d78e34b0c4fd5a0bf15e47e7bfebf39797bee075
ShashankVSS <s.shak101@gmail.com>
2024-04-30 13:16:45 -0400

    frame editor added

--- src/modify.py
+++ src/modify.py
@@ -7,6 +7,9 @@ class PoseRepublisher:
     def __init__(self):
         rospy.init_node('pose_republisher_node', anonymous=True)
         
+        # Read the target frame_id from the parameter server
+        self.target_frame_id = rospy.get_param('~target_frame_id', 'modified_frame')
+
         # Subscriber to the original PoseStamped topic
         self.subscriber = rospy.Subscriber('/original_pose', PoseStamped, self.pose_callback)
 
@@ -14,19 +17,20 @@ class PoseRepublisher:
         self.publisher = rospy.Publisher('/modified_pose', PoseStamped, queue_size=10)
 
     def pose_callback(self, data):
-        # Modify the received pose here (for example, increment position x by 1)
-        modified_pose = self.swap_xyz(data.pose)
+        # Modify the received pose here (for example, swap x, y, and z values)
+        modified_pose = self.modify_pose(data.pose)
 
         # Publish the modified pose
         self.publisher.publish(modified_pose)
 
-    def swap_xyz(self, pose):
-        # Swap x, y, and z values of the pose
+    def modify_pose(self, pose):
+        # Create a new PoseStamped message with modified values
         modified_pose = PoseStamped()
         modified_pose.header = pose.header
-        modified_pose.pose.position.x = pose.pose.position.y
-        modified_pose.pose.position.y = pose.pose.position.z
-        modified_pose.pose.position.z = pose.pose.position.x
+        modified_pose.header.frame_id = self.target_frame_id
+        modified_pose.pose.position.x = pose.pose.position.x  # Swapping x and y
+        modified_pose.pose.position.y = pose.pose.position.y  # Swapping y and z
+        modified_pose.pose.position.z = pose.pose.position.z  # Swapping z and x
         modified_pose.pose.orientation = pose.pose.orientation
         return modified_pose
 
